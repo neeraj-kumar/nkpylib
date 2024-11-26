c5b0c42773c80623a2356f9e8d94fcc260c0c08c
Neeraj Kumar <me@neerajkumar.org>
2024-11-26 18:06:34 -0500

    feat: Add cli_utils.py for command line utility functions

---
+++ cli_utils.py
@@ -0,0 +1,4 @@
+"""Command-line tools"""
+
+#TODO Refactor menu selection functionality: <action>:<items>, where <items> are single letters or ranges
+#TODO   see if it makes sense to implement this as a grammar
