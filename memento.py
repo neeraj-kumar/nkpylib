bc9c8426dc88569fe363166a407ae121961433d3
Neeraj Kumar <me@neerajkumar.org>
2025-03-03 21:35:54 -0500

    feat: Enhance MementoDB with key_field and automatic entry refresh logic

--- memento.py
+++ memento.py
@@ -7,6 +7,10 @@ from __future__ import annotations
 
 import json
 import os
+import re
+import time
+
+from typing import Any
 
 import requests
 
@@ -26,9 +30,9 @@ def memento_api(endpoint: str, method='get', **data):
     token = os.environ['MEMENTO_ACCESS_TOKEN']
     url = f'{base_url}/{endpoint}{conjunction}token={token}'
     url = f'{base_url}/{endpoint}'
-    # there's a limit of 10 reqs/min -> 6s delay
+    # there's a limit of 10 reqs/min, but we'll try pushing our luck a bit
     params = dict(**data, token=token)
-    r = make_request(url, method=method, min_delay=6, params=params)
+    r = make_request(url, method=method, min_delay=1, params=params)
     return r.json()
 
 def get_libraries() -> dict[str, str]:
@@ -95,9 +99,23 @@ def search_entries(q: str, library_id: str, **data) -> list[dict]:
 
 
 class MementoDB:
-    """Class to interact with the Memento database API."""
+    """Class to interact with a single library in the Memento database.
 
-    def __init__(self, name: str):
+    Initialize with the name (case-insensitive) of the library you want to work with.
+    We support the following key operations:
+    - iterate through entries
+    - add entries
+    - update entries
+    - lookup entries by text search
+
+    On first query, this will fetch all the entries and cache them. The cache can be updated
+    periodically, based on a provided value.
+
+    If you specify a `key_field` in the init, then you can use `__getitem__` to lookup entries and
+    `__contains__` to check if an entry exists.
+    """
+
+    def __init__(self, name: str, key_field: str='', update_interval_s=60*60):
         libraries = get_libraries()
         for lib_name, lib_id in libraries.items():
             if lib_name.lower() == name.lower():
@@ -106,24 +124,84 @@ class MementoDB:
         else:
             raise ValueError(f"Library '{name}' not found.")
         self.info = get_library(self.library_id)
+        self.key_field = key_field
+        self.update_interval_s = update_interval_s
+        self.last_update = 0
+        self.entries = []
 
+    def refresh(self):
+        """Refreshes the entries in the library if needed (based on last refresh time)."""
+        if time.time() - self.last_update > self.update_interval_s:
+            self.entries = get_entries(self.library_id)['entries']
+        self.last_update = time.time()
 
-    def get_entries(self, **data) -> list[dict]:
-        """Returns the entries in the library."""
-        return get_entries(self.library_id, **data)
+    def __len__(self) -> int:
+        """Returns the number of entries in the library."""
+        self.refresh()
+        return len(self.entries)
+
+    @classmethod
+    def key_compare(cls, key1: Any, key2: Any) -> bool:
+        """Compares two keys for equality. By default this is just a simple equality check."""
+        return key1 == key2
+
+    def __iter__(self):
+        """Iterates through the entries in the library.
+
+        Note that this first calls `refresh()` to update the entries if needed.
+        """
+        self.refresh()
+        if self.last_update > 0:
+            return iter(self.entries)
+
+    def __getitem__(self, key: Any) -> dict:
+        """Returns the first entry with the given key (based on the `key_field`).
+
+        If no matching entry is found, this raises a `KeyError`.
+        Note that this first calls `refresh()` to update the entries if needed.
+        """
+        #TODO cache the key lookup
+        if not self.key_field:
+            raise ValueError("You must set a `key_field` in the constructor to use this method.")
+        self.refresh()
+        for entry in self.entries:
+            if entry.get(self.key_field) == key:
+                return entry
+        raise KeyError(f"Key '{key}' not found in library.")
+
+    def __contains__(self, key: str) -> bool:
+        """Returns True if the given key is in the library (based on the `key_field`)."""
+        try:
+            _ = self[key]
+            return True
+        except KeyError:
+            return False
 
     def search_entries(self, q: str, **data) -> list[dict]:
-        """Searches the entries in the library for the given query `q`."""
+        """Searches the entries in the library for the given query `q`.
+
+        This version uses memento's own search functionality
+        """
         return search_entries(q, self.library_id, **data)
 
 
 if __name__ == '__main__':
     p = lambda x: print(json.dumps(x, indent=2))
-    r = get_libraries()
-    p(r)
-    info = get_library(r['movies'])
-    p(info)
-    #entries = get_entries(r['movies'])
-    entries = search_entries('tt0140352', r['movies'])
-    p(entries)
-    #p(entries['entries'][:10])
+    if 0:
+        r = get_libraries()
+        p(r)
+        info = get_library(r['movies'])
+        p(info)
+        #entries = get_entries(r['movies'])
+        entries = search_entries('tt0140352', r['movies'])
+        p(entries)
+        #p(entries['entries'][:10])
+    else:
+        movies = MementoDB('movies', key_field='imdb_link')
+        print(movies.info)
+        movies.refresh()
+        for i, m in enumerate(movies.entries[::-1]):
+            print(i, m)
+            if i > 5:
+                break
+
