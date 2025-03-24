23a5f813f2c98c2fd847924468be811128b82902
Neeraj Kumar <me@neerajkumar.org>
2025-03-24 15:35:45 -0400

    Added basic clustering to embeddings

--- ml/embeddings.py
+++ ml/embeddings.py
@@ -7,6 +7,7 @@ from collections.abc import Mapping
 import numpy as np
 
 from lmdbm import Lmdb
+from sklearn.cluster import AffinityPropagation, KMeans, AgglomerativeClustering
 from sklearn.preprocessing import StandardScaler
 from tqdm import tqdm
 
@@ -24,10 +25,16 @@ class Embeddings(Mapping):
         """
         self.path = path
         self.db = Lmdb.load(path, mode)
+        # we cache the order of our keys
+        self._keys = [k.decode('utf-8') for k in self.db.keys()]
+        self.n_dims = 0
+        for key, value in self.items():
+            self.n_dims = len(value)
+            break
+        self.cached = dict()
 
     def __iter__(self):
-        for key in self.db:
-            yield key.decode('utf-8')
+        return iter(self._keys)
 
     def __len__(self):
         return len(self.db)
@@ -52,12 +59,52 @@ class Embeddings(Mapping):
                             normed: bool=False,
                             scale_mean:bool=True,
                             scale_std:bool=True) -> tuple[list[str], 2darray]:
-        """Returns a list of string keys and a numpy array of embeddings."""
+        """Returns a list of string keys and a numpy array of embeddings.
+
+        You can optionally set the following flags:
+        - `normed`: Normalize embeddings to unit length.
+        - `scale_mean`: Scale embeddings to have zero mean.
+        - `scale_std`: Scale embeddings to have unit variance.
+
+        The keys and embeddings are cached for future calls with the same flags.
+        """
+        cache_kw = dict(normed=normed, scale_mean=scale_mean, scale_std=scale_std)
+        if self.cached and all(self.cached[k] == v for k, v in cache_kw.items()):
+            return self.cached['keys'], self.cached['embs']
         keys, embs = zip(*list(self.items()))
+        keys = list(keys)
         embs = np.vstack(embs)
         if normed:
             embs = embs / np.linalg.norm(embs, axis=1)[:, None]
         if scale_mean or scale_std:
             scaler = StandardScaler(with_mean=scale_mean, with_std=scale_std)
             embs = scaler.fit_transform(embs)
+        # cache these
+        self.cached.update(keys=keys, embs=embs, **cache_kw)
         return keys, embs
+
+    def cluster(self, n_clusters=-1, method='kmeans', **kwargs) -> list[list[str]]:
+        """Clusters our embeddings.
+
+        If `n_clusters` is not positive (default), we set it to the sqrt of the number of
+        embeddings we have.
+
+        Returns a list of lists of keys, where each list is a cluster; in order from largest to smallest.
+        """
+        keys, embs = self.get_keys_embeddings(normed=False, scale_mean=True, scale_std=True)
+        if n_clusters <= 0:
+            n_clusters = int(np.sqrt(len(keys)))
+        if method == 'kmeans':
+            clusterer = KMeans(n_clusters=n_clusters)
+        elif method in ('agg', 'average'):
+            clusterer = AgglomerativeClustering(n_clusters=n_clusters, linkage='average')
+        elif method == 'affinity':
+            clusterer = AffinityPropagation()
+        else:
+            raise NotImplementedError(f'Clustering method {method!r} not implemented.')
+        labels = clusterer.fit_predict(embs)
+        uniques = set(labels)
+        clusters = {i: [] for i in uniques}
+        for key, label in zip(keys, labels):
+            clusters[label].append(key)
+        return sorted(clusters.values(), key=len, reverse=True)
