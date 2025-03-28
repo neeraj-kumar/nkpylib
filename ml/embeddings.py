3e6124333a21fe45d5a0455f04ccecfbf59225e4
Neeraj Kumar <me@neerajkumar.org>
2025-03-28 16:02:03 -0400

    Fixed various issues in embeddings code, and added an explicit make_classifier func

--- ml/embeddings.py
+++ ml/embeddings.py
@@ -12,14 +12,17 @@ from typing import Any, Sequence
 import numpy as np
 
 from lmdbm import Lmdb
-from sklearn.cluster import AffinityPropagation, KMeans, AgglomerativeClustering
+from sklearn.cluster import AffinityPropagation, KMeans, AgglomerativeClustering, MiniBatchKMeans
 from sklearn.linear_model import SGDClassifier
 from sklearn.neighbors import NearestNeighbors
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
 
-array1d = np.ndarray | Sequence[float]
-array2d = np.ndarray | Sequence[Sequence[float]]
+nparray1d = np.ndarray
+nparray2d = np.ndarray
+
+array1d = nparray1d | Sequence[float]
+array2d = nparray2d | Sequence[Sequence[float]]
 
 logger = logging.getLogger(__name__)
 
@@ -34,7 +37,7 @@ class Embeddings(Mapping):
         This must be LMDB format for now.
         """
         self.path = path
-        self.db = Lmdb.load(path, mode)
+        self.db = Lmdb.open(path, mode)
         # we cache the order of our keys
         self._keys = [k.decode('utf-8') for k in self.db.keys()]
         self.n_dims = 0
@@ -108,7 +111,7 @@ class Embeddings(Mapping):
         if n_clusters <= 0:
             n_clusters = int(np.sqrt(len(keys)))
         if method == 'kmeans':
-            clusterer = KMeans(n_clusters=n_clusters)
+            clusterer = MiniBatchKMeans(n_clusters=n_clusters)
         elif method in ('agg', 'average'):
             clusterer = AgglomerativeClustering(n_clusters=n_clusters, linkage='average')
         elif method == 'affinity':
@@ -124,43 +127,33 @@ class Embeddings(Mapping):
 
     def similar(self,
                 queries: list[str]|array2d,
-                weights: list[float]|None,
+                weights: list[float]|None=None,
                 n_neg: int=1000,
                 method: str='rbf',
-                C=10,
+                min_score: float=-0.1,
                 **kw) -> list[tuple[float, str]]:
         """Returns the most similar keys and scores to the given `queries`.
 
-        The queries can either be keys from this class, or embedding vectors.
+        This is a wrapper on top of `nearest_neighbors()` and `make_classifier()`.
 
-        If `weights` is provided, it should be of the same length as `keys` and is a weight for key. These can be negative as well.
+        The queries can either be keys from this class, or embedding vectors.
 
         Returns (score, key) tuples in descending order of score.
         """
-        if weights is not None:
-            assert len(queries) == len(weights), f'Length of weights {len(weights)} must match queries {len(queries)}'
         assert len(queries) > 0, 'Must provide at least one query.'
         assert len({type(q) for q in queries}) == 1, 'All queries must be of the same type.'
         if isinstance(queries[0], str):
             assert all(q in self for q in queries), f'All queries must be in the embeddings.'
-        keys, embs = self.get_keys_embeddings(normed=True, scale_mean=False, scale_std=False)
+        #TODO normalize queries if not in dataset
+        keys, embs = self.get_keys_embeddings(normed=True, scale_mean=False, scale_std=True)
         pos: Any
         if method == 'nn':
-            # use nearest neighbors, summed over all queries
-            nn = NearestNeighbors(n_neighbors=n_neg, metric='cosine')
-            nn.fit(embs)
             if isinstance(queries[0], str):
                 _pos = np.array([i for i, k in enumerate(keys) if k in queries])
                 pos = embs[_pos]
             else:
                 pos = queries
-            scores, indices = nn.kneighbors(pos, n_neg, return_distance=True)
-            # aggregate scores for each index over all queries
-            score_by_index: Counter = Counter()
-            for i, s in zip(indices, scores):
-                for j, k in zip(i, s): # for each query, add the score to the index
-                    score_by_index[j] += k
-            _ret = [(score, keys[idx]) for idx, score in score_by_index.most_common()]
+            _ret = self.nearest_neighbors(pos, n_neighbors=n_neg, **kw)
         else:
             # train a classifier with these as positive and some randomly chosen as negative
             if isinstance(queries[0], str):
@@ -173,20 +166,55 @@ class Embeddings(Mapping):
                 neg = random.sample(range(len(embs)), n_neg)
                 X = np.vstack([queries, embs[neg]])
             y = [1]*len(pos) + [-1]*len(neg)
-            assert len(X) == len(y), f'Length of X {len(X)} must match y {len(y)}'
-            logger.debug(f'training: {pos+neg}, {X.shape}, {X}, {y}')
-            clf_kw = dict(class_weight='balanced', C=C)
-            if method == 'rbf':
-                clf = SVC(kernel='rbf', **clf_kw)
-            elif method == 'linear':
-                clf = SVC(kernel='linear', **clf_kw)
-            elif method == 'sgd':
-                clf = SGDClassifier(**clf_kw)
-            else:
-                raise NotImplementedError(f'Classifier method {method!r} not implemented.')
-            clf.fit(X, y, sample_weight=weights)
+            clf = self.make_classifier(X, y, method=method, **kw)
             scores = clf.decision_function(embs)
-            _ret = [(s, k) for s, k in zip(scores, keys) if s > 0]
+            logger.debug(f'Got scores {scores.shape}: {scores}')
+            _ret = [(s, k) for s, k in zip(scores, keys) if s > min_score]
+            logger.debug(f'got _ret: {len(_ret)}: {_ret[:10]}')
         # sort results by score (desc) and filter out queries
         ret = sorted([(float(s), k) for s, k in _ret if k not in queries], reverse=True)
         return ret
+
+    def nearest_neighbors(self, pos: array2d, n_neighbors:int=1000, metric='cosine', **kw):
+        """Runs nearest neighbors with given `pos` embeddings, aggregating scores."""
+        nn = NearestNeighbors(n_neighbors=n_neighbors, metric=metric)
+        keys, embs = self.get_keys_embeddings(normed=True, scale_mean=False, scale_std=True)
+        nn.fit(embs)
+        scores, indices = nn.kneighbors(pos, n_neighbors, return_distance=True)
+        # aggregate scores for each index over all queries
+        score_by_index: Counter = Counter()
+        for i, s in zip(indices, scores):
+            for j, k in zip(i, s): # for each query, add the score to the index
+                score_by_index[j] += k
+        ret = [(score, keys[idx]) for idx, score in score_by_index.most_common()]
+        return ret
+
+    def make_classifier(self,
+                        X: nparray2d,
+                        y: Sequence[float|int],
+                        weights: Sequence[float]|None=None,
+                        method: str='rbf',
+                        C=10,
+                        class_weight='balanced',
+                        **kw) -> Any:
+        """Makes a classifier with given `method`, trains it on X, y, and returns it.
+
+        If `weights` is provided, it should be of the same length as `keys` and is a weight for key.
+        These can be negative as well.
+
+        """
+        assert len(X) == len(y), f'Length of X {len(X)} must match y {len(y)}'
+        if weights is not None:
+            assert len(X) == len(weights), f'Length of weights {len(weights)} must match X {len(X)}'
+        logger.debug(f'training labels {Counter(y).most_common()}, X: {X.shape}, {X}')
+        clf_kw = dict(class_weight=class_weight, **kw)
+        if method == 'rbf':
+            clf = SVC(kernel='rbf', C=C, **clf_kw)
+        elif method == 'linear':
+            clf = SVC(kernel='linear', C=C, **clf_kw)
+        elif method == 'sgd':
+            clf = SGDClassifier(**clf_kw)
+        else:
+            raise NotImplementedError(f'Classifier method {method!r} not implemented.')
+        clf.fit(X, y, sample_weight=weights)
+        return clf
