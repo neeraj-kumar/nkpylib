c15ad6d17ed223d8a4372c8478cdbea47e148cdc
Neeraj Kumar <me@neerajkumar.org>
2025-04-29 11:48:29 -0400

    Updated default user agent

--- constants.py
+++ constants.py
@@ -2,8 +2,8 @@
 
 import re
 
-# as of sep 26, 2024
-USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:130.0) Gecko/20100101 Firefox/130.0'
+# as of apr 29, 2025
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:137.0) Gecko/20100101 Firefox/137.0'
 
 URL_REGEXP = re.compile(
     r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
