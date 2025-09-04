4639e46019c65e98a33985f18576d35af576680f
Neeraj Kumar <me@neerajkumar.org>
2025-09-04 09:59:22 -0400

    My old regexp for detecting urls seemed broken, so i replaced it with a much simpler one

--- constants.py
+++ constants.py
@@ -5,6 +5,9 @@ import re
 # as of apr 29, 2025
 USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:137.0) Gecko/20100101 Firefox/137.0'
 
-URL_REGEXP = re.compile(
+OLD_URL_REGEXP = re.compile(
     r"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"
 )
+
+# very simple url regexp
+URL_REGEXP = re.compile(r"https?:\/\/\S+")
