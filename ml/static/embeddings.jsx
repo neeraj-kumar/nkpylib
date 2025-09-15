ffe2cfae0cba49334aec3526a5066dadb244cda6
Neeraj Kumar <me@neerajkumar.org>
2025-09-15 14:28:31 -0400

    feat: add initial embeddings page with index data fetching

--- ml/static/embeddings.jsx
+++ ml/static/embeddings.jsx
@@ -1,7 +1,26 @@
 // Note that this is imported in the main HTML file directly, so you have to use React.xxx for everything
 
+// A view is one view into our data. We can create and delete them as needed.
+const View = ({}) => {
+  return (<div>View</div>);
+}
+
 const Main = () => {
-  return <div>Embeddings Page</div>;
+  const [ids, setIds] = React.useState([]);
+  const [views, setViews] = React.useState([]);
+  React.useEffect(() => {
+    fetch("/index/")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('Got index data', data);
+        setIds(data.ids);
+      });
+  }, []);
+
+  return (<div>
+    <h3>Embeddings Page</h3>
+    <p>{ids.length} pts: {ids.slice(0, 5).join(', ')}</p>
+  </div>);
 };
 
 ReactDOM.createRoot(document.getElementById("main")).render(<Main />);
