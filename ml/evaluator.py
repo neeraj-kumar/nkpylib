a72cfa387cdf05f31c66fe8875e278dd84ee15ea
Neeraj Kumar <me@neerajkumar.org>
2025-09-19 09:29:31 -0400

    Can now parse tags into different kinds of label sets

--- ml/evaluator.py
+++ ml/evaluator.py
@@ -17,6 +17,7 @@ from argparse import ArgumentParser
 from collections.abc import Mapping
 from collections import Counter, defaultdict
 from os.path import abspath, dirname, exists, join
+from pprint import pprint
 from typing import Any, Sequence, Generic, TypeVar
 
 import numpy as np
@@ -44,13 +45,108 @@ from nkpylib.ml.tag_db import Tag, get_all_tags, init_tag_db
 
 logger = logging.getLogger(__name__)
 
+FLOAT_TYPES = (float, np.float32, np.float64)
+NUMERIC_TYPES = FLOAT_TYPES + (int, np.int32, np.int64)
+
+class Labels:
+    """A base class for different types of labels that we get from tags.
+
+    This stores metadata about the types of labels, and the specialized values.
+    """
+    def __init__(self, tag_type: str, key: str, *, ids: list[str], values: Any):
+        self.tag_type = tag_type
+        self.key = key
+        self.ids = ids
+        self.values = values
+
+    def __repr__(self):
+        return f'<{self.__class__.__name__} {self.tag_type} {self.key} ({len(self.ids)} labels)>'
+
+class NumericLabels(Labels):
+    """A class for numeric labels. For now we convert them to floats."""
+    def __init__(self, tag_type: str, key: str, ids_values: list[tuple[str, Any]]):
+        ids = [id for id, v in ids_values]
+        values = np.array([v for id, v in ids_values], dtype=np.float32)
+        super().__init__(tag_type, key, ids=ids, values=values)
+
+class MulticlassLabels(Labels):
+    """A class for multiclass (mutually-exclusive) labels."""
+    def __init__(self, tag_type: str, key: str, ids_values: list[tuple[str, Any]]):
+        ids = [id for id, v in ids_values]
+        values = [v for id, v in ids_values]
+        types = Counter(type(v) for v in values)
+        assert len(types) == 1
+        t = type(values[0])
+        assert t not in FLOAT_TYPES, f'No floats in multiclass label: {types}'
+        super().__init__(tag_type, key, ids=ids, values=values)
+
+class MultilabelLabels(Labels):
+    """A class for multilabel (non-mutually-exclusive) labels."""
+    def __init__(self, tag_type: str, key: str, ids_values: list[tuple[str, Any]]):
+        ids = set()
+        values = defaultdict(list)
+        types = Counter()
+        for id, v in ids_values:
+            ids.add(id)
+            values[id].append(v)
+            types[type(v)] += 1
+        ids = sorted(ids)
+        assert len(types) == 1
+        t = type(values[0])
+        assert t not in FLOAT_TYPES, f'Not floats in multilabel label: {types}'
+        super().__init__(tag_type, key, ids=ids, values=values)
+
+
+def parse_into_labels(tag_type: str,
+                      key: str,
+                      ids_values: list[tuple[str, Any]],
+                      impure_thresh=0.1) -> Labels:
+    """Parses our (id, value) pairs into a Labels object of the appropriate type."""
+    ids = [id for id, v in ids_values]
+    values = [v for id, v in ids_values]
+    types = Counter(type(v) for v in values)
+    most_t, n_most = types.most_common(1)[0]
+    print(f'For {(tag_type, key)} got {len(ids)} ids, types: {types.most_common()}')
+    # if we have less than `impure_thresh` of other types, ignore them
+    if len(types) > 1:
+        impure = 1.0 - (n_most / len(ids))
+        logger.debug(f'  Most common (purity): {n_most}/{len(ids)} -> {impure}')
+        if impure < impure_thresh:
+            new_ids_values = [(id, v) for id, v in ids_values if type(v) == most_t]
+            return parse_into_labels(tag_type, key, new_ids_values, impure_thresh=impure_thresh)
+        else:
+            raise NotImplementedError(f'Cannot handle mixed types: {types.most_common()}')
+    # at this point we should have exactly one type
+    #print(f'Got {len(ids)} ids, {len(set(ids))} unique ids, type: {most_t}')
+    if len(set(ids)) != len(ids): # we have duplicate ids
+        # check for impurity level
+        impure = 1.0 - (len(set(ids)) / len(ids))
+        logger.debug(f'  Multilabel impurity {impure}: {len(set(ids))}/{len(ids)}')
+        if impure < impure_thresh:
+            seen_ids = set()
+            new_ids_values = []
+            for id, v in ids_values:
+                if id in seen_ids:
+                    continue
+                seen_ids.add(id)
+                new_ids_values.append((id, v))
+            ids_values = new_ids_values
+        else:
+            return MultilabelLabels(tag_type, key, ids_values)
+    if most_t in NUMERIC_TYPES: # numeric
+        return NumericLabels(tag_type, key, ids_values)
+    else: # categorical
+        return MulticlassLabels(tag_type, key, ids_values)
+
+
+
 class EmbeddingsValidator:
     """A class to validate/evaluate embeddings in various ways, semi-automatically."""
     def __init__(self, paths: list[str], tag_path: str, **kw):
         self.paths = paths
         self.fs = FeatureSet(paths, **kw)
+        self.labels = {}
         self.parse_tags(tag_path)
-        self.tags = {}
 
     def parse_tags(self, tag_path: str) -> None:
         """Parses our tags from the tag db"""
@@ -60,8 +156,15 @@ class EmbeddingsValidator:
             # get all tags, group by (type, key)
             tags = Tag.select()
             for t in Tag.select():
-                grouped[(t.type, t.key)].append((t.id, t.value))
-        logger.info(f'Loaded {len(grouped)} types of tags from {tag_path}: {list(grouped.keys())}')
+                key = (t.type, t.key)
+                v = specialize(t.value)
+                grouped[key].append((t.id, v))
+        logger.info(f'Loaded {len(grouped)} types of tags from {tag_path}')
+        for (tag_type, key), ids_values in grouped.items():
+            if 'revenue' in key:
+                print(f'{(tag_type, key)} -> {ids_values[:20]}')
+            self.labels[key] = parse_into_labels(tag_type, key, ids_values)
+        pprint(self.labels)
 
     def run(self) -> None:
         logger.info(f'Validating embeddings in {self.paths}, {len(self.fs)}')
