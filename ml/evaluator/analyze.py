b53b930eb592d4a540a788add9a2073d49fa3f86
Neeraj Kumar <me@neerajkumar.org>
2025-10-04 10:43:44 -0400

    refactor: Improve provenance analysis and result exploration logic

--- ml/evaluator/analyze.py
+++ ml/evaluator/analyze.py
@@ -12,7 +12,7 @@ from glob import glob
 from os.path import dirname, abspath, join
 from subprocess import run
 
-from objexplore import explore
+from objexplore import explore # type: ignore
 
 from nkpylib.ml.feature_set import JsonLmdb
 
@@ -27,57 +27,40 @@ def get_latest_result_path(dir=RESULTS_DIR) -> str:
         raise FileNotFoundError('No results files found in results directory')
     return result_files[-1]
 
+
+def build_provenance_analysis(result_data: dict[str, Any], db: JsonLmdb):
+    """Build analysis dict with full provenance chain."""
+    provenance_keys = result_data.get('provenance', [])
+    all_keys = provenance_keys + [result_data['key']]
+    chain_results = []
+    by_end = [f'key:{k}' for (k, _) in db['by:end_time']]
+    for key in all_keys:
+        full_key = f'key:{key}' if not key.startswith('key:') else key
+        if full_key in db:
+            chain_results.append((key, db[full_key]))
+            if key not in by_end:
+                by_end.append(key)
+    chain_results.sort(key=lambda pair: by_end.index(pair[0]))
+    ret = {}
+    for key, result in chain_results:
+        ret[key] = {
+            'op': result.get('op_name', ''),
+            'key': key,
+            'analysis': result.get('analysis', {}),
+            'variant': result.get('variant', ''),
+            'instance_vars': result.get('instance_vars', {}),
+        }
+    return ret
+
 def explore_results(db: JsonLmdb, **kw):
     """Uses visidata to explore the results database."""
     # get result keys ordered by end time
     by_end = [f'key:{k}' for (k, _) in db['by:end_time']]
     # get all results, filtered down
-    results = {k: db[k] for k in by_end[:200]}
-    
-    # Build provenance chain analysis for each result
-    def build_provenance_analysis(result_data):
-        """Build analysis dict with full provenance chain."""
-        provenance_keys = result_data.get('provenance', [])
-        all_keys = provenance_keys + [result_data['key']]
-        
-        # Get all results in provenance chain
-        chain_results = []
-        for key in all_keys:
-            full_key = f'key:{key}' if not key.startswith('key:') else key
-            if full_key in db:
-                chain_results.append((key, db[full_key]))
-        
-        # Sort by end_ts
-        def get_end_ts(item):
-            _, result = item
-            timestamps = result.get('timestamps', {})
-            return timestamps.get('finished', timestamps.get('end_time', 0))
-        
-        chain_results.sort(key=get_end_ts)
-        
-        # Build analysis dict
-        analysis_dict = {}
-        for key, result in chain_results:
-            # Get instance params from the result
-            instance_params = {}
-            if 'variant' in result and result['variant']:
-                # Try to extract params from variant name or other fields
-                instance_params['variant'] = result['variant']
-            
-            analysis_dict[key] = {
-                'op': result.get('op_name', ''),
-                'key': key,
-                'analysis': result.get('analysis', {}),
-                'variant': result.get('variant', ''),
-                'instance_params': instance_params
-            }
-        
-        return analysis_dict
-    
-    # Replace analysis field for each result
+    results = {k: db[k] for k in by_end[:300]}
+    # get full provenance analysis
     for k, result_data in results.items():
-        results[k] = {**result_data, 'analysis': build_provenance_analysis(result_data)}
-    
+        results[k] = {**result_data, 'analysis': build_provenance_analysis(result_data, db=db)}
     logger.info(f'Exploring {len(results)} results')
     #explore(results, **kw)
     # write to a tempfile then run visidata on it
@@ -104,4 +87,4 @@ if __name__ == '__main__':
     kw = vars(args)
     kw.pop('result_path')
     func = kw.pop('func')
-    funcs[func](db=db, **kw)
+    funcs[func](db=db, **kw) # type: ignore
