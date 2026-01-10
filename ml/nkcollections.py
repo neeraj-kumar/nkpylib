632bd031c2e9ee3d4492875a9d569bf11d2194ec
Neeraj Kumar <me@neerajkumar.org>
2026-01-10 04:19:04 -0500

    Collections code now allows downloading files and extracting text and image embeddings

--- ml/nkcollections.py
+++ ml/nkcollections.py
@@ -12,7 +12,7 @@ import time
 
 from argparse import ArgumentParser
 from collections import defaultdict
-from os.path import abspath, join, dirname
+from os.path import abspath, exists, join, dirname
 from pprint import pprint
 from queue import Queue, Empty
 from threading import Thread
@@ -21,16 +21,42 @@ import termcolor
 import tornado.web
 
 from tornado.web import RequestHandler
-from pony.orm import * # type: ignore
-from pony.orm.core import Entity, EntityMeta, SetInstance # type: ignore
+from pony.orm import (
+    composite_index,
+    Database,
+    db_session,
+    Json,
+    Optional,
+    PrimaryKey,
+    Required,
+    Set,
+) # type: ignore
 
 from nkpylib.nkpony import sqlite_pragmas, GetMixin
+from nkpylib.ml.nklmdb import NumpyLmdb, batch_extract_embeddings
 from nkpylib.stringutils import parse_num_spec
-from nkpylib.web_utils import BaseHandler, simple_react_tornado_server
+from nkpylib.web_utils import BaseHandler, simple_react_tornado_server, make_request
 
 logger = logging.getLogger(__name__)
 
 sql_db = Database()
+
+def maybe_dl(url: str, path: str) -> bool:
+    """Downloads the given url to the given dir if it doesn't already exist there (and is not empty).
+
+    Returns if we actually downloaded the file.
+    """
+    if exists(path) and os.path.getsize(path) > 0:
+        return False
+    logger.debug(f'downloading image {url} -> {path}')
+    r = make_request(url, headers={'Accept': 'image/*,video/*'})
+    try:
+        os.makedirs(dirname(path), exist_ok=True)
+    except Exception as e:
+        pass
+    with open(path, 'wb') as f:
+        f.write(r.content)
+    return True
 
 class Collection(sql_db.Entity, GetMixin):
     id = PrimaryKey(int, auto=True)
@@ -44,6 +70,42 @@ class Collection(sql_db.Entity, GetMixin):
     md = Optional(Json)
     children = Set('Collection', reverse='parent')
     composite_index(source, stype, otype)
+
+    @classmethod
+    def update_embeddings(cls, lmdb_path: str, images_dir: str, **kw) -> int:
+        """Updates the embeddings for all relevant rows in our table.
+
+        The embeddings are stored in a NumpyLmdb at the given `lmdb_path`.
+        For images, we first download them to the given `images_dir`.
+
+        Any kw are passed to `batch_extract_embeddings`.
+
+        We return the number of embeddings updated.
+        """
+        # first do text rows
+        rows = cls.select(lambda c: c.otype == 'text')
+        logger.info(f'Updating embeddings for upto {rows.count()} text rows: {rows[:5]}...')
+        inputs = [(str(c.id), c.md['text']) for c in rows if c.md and 'text' in c.md]
+        def md_func(key, input):
+            return dict(embedding_ts=int(time.time()))
+
+        n_text = batch_extract_embeddings(inputs=inputs, db_path=lmdb_path, embedding_type='text', md_func=md_func, **kw)
+        logger.info(f'  Updated embeddings for {n_text} text rows')
+        # now do image rows. first we have to download them.
+        rows = cls.select(lambda c: c.otype == 'image')
+        logger.info(f'Updating embeddings for upto {rows.count()} image rows: {rows[:5]}...')
+        inputs = []
+        for c in rows:
+            url = c.url
+            ext = url.split('.')[-1]
+            key = c.md.get('media_key', c.id)
+            path = abspath(join(images_dir, f'{key}.{ext}'))
+            downloaded = maybe_dl(url, path)
+            inputs.append((str(c.id), path))
+        n_image = batch_extract_embeddings(inputs=inputs, db_path=lmdb_path, embedding_type='image', md_func=md_func)
+        logger.info(f'  Updated embeddings for {n_image} image rows')
+        return n_text + n_image
+
 
 def init_sql_db(path: str) -> Database:
     """Initializes the sqlite database at the given `path`"""
@@ -77,4 +139,5 @@ def web_main():
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(funcName)s:%(lineno)d - %(levelname)s - %(message)s")
     web_main()
